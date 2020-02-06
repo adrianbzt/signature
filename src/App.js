927697a58c9f982805d02e18b3a9538913a91496
Adrian Buzatu <adrian.buzatu@gameloft.com>
2020-02-06 17:08:37 +0200

    save svg

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
                              src="https://s.ftcdn.net/v2013/pics/all/curated/RKyaEDwp8J7JKeZWQPuOVWvkUjGQfpCx_cover_580.jpg?r=1a0fc22192d0c808b8bb2b9bcfbf4a45b1793687"
                              alt="ceva imagine"/>
                 <SignatureCanvas/>
+
                 <ClockComponent/>
             </header>
         </div>
--- src/components/SignatureCanvas.js
+++ src/components/SignatureCanvas.js
@@ -7,13 +7,21 @@ class Signature extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            show: true,
+            show: false,
             count: 0,
+            srcElement: 'canvas-test',
+            buttons: {
+                sign: false,
+                clear: false,
+                revoke: true
+            },
+            signInput: null
         }
         this.incrementCount = this.incrementCount.bind(this);
         this.doMagic = this.doMagic.bind(this);
         this.clearCanvas = this.clearCanvas.bind(this);
         this.exportSignature = this.exportSignature.bind(this);
+        this.sign = this.sign.bind(this);
     }
 
     componentDidMount() {
@@ -24,20 +32,21 @@ class Signature extends React.Component {
             <div>
 
                 {this.state.show ?
-                <SignaturePad width={600} height={300} options={
-                    {
-                        minWidth: 2,
-                        maxWidth: 5,
-                        penColor: 'rgb(0,0,0)',
-                        dotSize: 5,
-                        throttle: 10,
-                        minDistance: 10,
-                        backgroundColor: 'rgb(255,255,255)',
-                        velocityFilterWeight: 1,
-                    }
-                }/>
+                    <SignaturePad width={600} height={300} options={
+                        {
+                            minWidth: 2,
+                            maxWidth: 5,
+                            penColor: 'rgb(0,0,0)',
+                            dotSize: 5,
+                            throttle: 10,
+                            minDistance: 10,
+                            backgroundColor: 'rgb(255,255,255)',
+                            velocityFilterWeight: 1,
+                        }
+                    }/>
                     : null}
 
+                <Button variant="contained" color="primary" onClick={this.sign}> Sign</Button>
                 <Button variant="contained" color="primary" onClick={this.doMagic}> Clear Me</Button>
                 <Button variant="contained" color="secondary" onClick={this.exportSignature}> Save SVG</Button>
 
@@ -53,6 +62,10 @@ class Signature extends React.Component {
         this.clearCanvas();
     }
 
+    sign() {
+        this.setState({show: true})
+    }
+
     incrementCount() {
         this.setState((state) => {
             return {count: state.count + 1}
@@ -60,17 +73,25 @@ class Signature extends React.Component {
     }
 
     clearCanvas() {
-        this.setState({ show: false });
-        setTimeout( () => { this.setState({ show: true }); }, 0);
+        this.setState({show: false});
+        setTimeout(() => {
+            this.setState({show: true});
+        }, 0);
     }
 
     exportSignature() {
-        alert('Work in progress')
+
+        let signature = document.getElementsByTagName('canvas')[0]
+
+        var image = signature.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+
+        window.location.href=image; // it will save locally
+
+
     }
 
 
 }
-
 
 
 export default Signature;
